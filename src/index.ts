0a2551cb23b14903bf79909219c2dd05de2e6f47
guxiaoyao <>
2019-10-06 05:54:39 +0800

    feat(完成发送请求前初始化):

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+import { buildUrl } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
+  xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transfromURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transfromURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildUrl(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type Method = 'get' | 'GET' | 'delete' | 'DELETE' | 'head' | 'HEAD' | 'option' | 'OPTION' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+}
